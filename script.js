9b1dd784cd0f2264d9a7ad85a931274e4c08357b
adesanto <aarondesanto@gmail.com>
2015-11-11 16:27:11 -0500

    repeating properly. every element shows up only once per page.

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
 
-
 $.get('https://www.reddit.com/r/AmazingTechnology/.json', function(response) {
 	
-	var post = response.data.children;
+	var posts = response.data.children;
 	
-	function someFunc(num, prop) {
-		console.log(post[num].data[prop]);
-	}
-	console.log(response.data.children[0].data);
-	someFunc(0, "thumbnail");
-	someFunc(0, "author");
-	someFunc(0, "url");
-	someFunc(0, "title");
+	$.each(posts, function(index, each) {
+		var $div = $("<div class='someClass' />").appendTo($('body'));
+		$div.append("<h1>" + each.data.title + "</h1>");
+		$div.append("<a href='" + each.data.url + "'><img src='" + each.data.thumbnail + "'></a>");
+		$div.append("<h3>" + each.data.author + "</h3>");
+	});
+	
 });
 
 });
