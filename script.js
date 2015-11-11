76bb7c32081f8e81a13aa032669477e3636a3e41
adesanto <aarondesanto@gmail.com>
2015-11-11 14:42:56 -0500

    properly talking to reddit now

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 $(document).ready(function() {
 
+
 $.get('https://www.reddit.com/r/AmazingTechnology/.json', function(response) {
+	
+	var post = response.data.children;
+	
+	function someFunc(num, prop) {
+		console.log(post[num].data[prop]);
+	}
 	console.log(response.data.children[0].data);
+	someFunc(0, "thumbnail");
+	someFunc(0, "author");
+	someFunc(0, "url");
+	someFunc(0, "title");
 });
 
 });
