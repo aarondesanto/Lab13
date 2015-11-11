44127c2169ed93a46865b03d0c408b48a9db6507
adesanto <aarondesanto@gmail.com>
2015-11-11 16:53:41 -0500

    some style changes

--- script.js
+++ script.js
@@ -8,8 +8,10 @@ $.get('https://www.reddit.com/r/AmazingTechnology/.json', function(response) {
 		var $div = $("<div class='someClass' />").appendTo($('body'));
 		$div.append("<h1>" + each.data.title + "</h1>");
 		$div.append("<a href='" + each.data.url + "'><img src='" + each.data.thumbnail + "'></a>");
-		$div.append("<h3>" + each.data.author + "</h3>");
+		$div.append("<h3> Posted by: <span class='author'>" + each.data.author + "</span></h3>");
 	});
+	
+	$('img').attr('alt', "Click here to read this article.")
 	
 });
 
