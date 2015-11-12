de5d8e3805f98673f07564201a889c694d2c9e29
adesanto <aarondesanto@gmail.com>
2015-11-12 01:06:13 -0500

    cleaned up the css a lot. restyled some elements. added comments button.

--- script.js
+++ script.js
@@ -4,22 +4,24 @@ $.get('https://www.reddit.com/r/ImaginaryTechnology/.json', function(response) {
 	
 	var posts = response.data.children;
 	
+	$('<header />').append("Super Cool 90s Reddit Website").prependTo($('body'));
+	
 	$.each(posts, function(index, post) {
 		
 		var $postLink = $('<a />').attr({ class: 'postLink', href: post.data.url, target: '_blank' }).appendTo($('body'));
 		
 		var $div = $('<div />').attr('class', 'postWrapper').appendTo($postLink);
 		
-		var $title = $('<h1 />').append(post.data.title);
+		var $title = $('<h1 />').append(post.data.title).appendTo($div);
 		
-		var $thumb = $('<img />').attr({ class: 'thumbs', src: post.data.thumbnail, alt: 'No Image Preview Available' });
+		var $thumb = $('<img />').attr({ class: 'thumbs', src: post.data.thumbnail, alt: 'No Image Preview Available' }).appendTo($div);
 		
-		var $author = $('<h3 />').append("Posted by: ");
+		var $author = $('<h3 />').append("Posted by: ").appendTo($div);
 		var $authorSpan = $('<span />').attr('class', 'authorSpan').append(post.data.author).appendTo($author);
 		
-		var $divImg = $('<img />').attr({ class: 'dividerImg', src: 'images/divider.gif' });
+		var $comments = $('<a />').attr({ class: 'commentLink', href: 'http://www.reddit.com/' + post.data.permalink }).append(" View Comments ").appendTo($div);
 		
-		$div.append($title, $thumb, $author, $divImg);
+		var $divImg = $('<img />').attr({ class: 'dividerImg', src: 'images/divider.gif' }).appendTo($div);
 		
 	});
 	
